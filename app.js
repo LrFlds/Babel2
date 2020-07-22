050be6f69e4747c517cde5d5f2635131cb1fe85e
LrFlds <64799791+LrFlds@users.noreply.github.com>
2020-07-22 11:59:31 +0200

    BABEL

---
+++ app.js
@@ -0,0 +1,33 @@
+const bouton1 = document.querySelector('#card1');
+bouton1.addEventListener('click', open);
+    function open(){
+        const popup = document.querySelector('.popup');
+        popup.classList.add('popup-actif');
+    }
+    const boutonClose = document.querySelector('.close');
+boutonClose.addEventListener('click', close);
+    function close(){
+        const popup = document.querySelector('.popup');
+        popup.classList.remove('popup-actif');
+    }
+
+    const bouton2 = document.querySelector('#card2');
+    bouton2.addEventListener('click', open);
+        function open(){
+            const popup = document.querySelector('.popup');
+            popup.classList.add('popup-actif');
+        }
+
+        const bouton3 = document.querySelector('#card3');
+        bouton3.addEventListener('click', open);
+            function open(){
+                const popup = document.querySelector('.popup');
+                popup.classList.add('popup-actif');
+            }
+            
+            const bouton4 = document.querySelector('#card4');
+    bouton4.addEventListener('click', open);
+        function open(){
+            const popup = document.querySelector('.popup');
+            popup.classList.add('popup-actif');
+        }
